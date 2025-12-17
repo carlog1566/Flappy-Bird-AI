c3d341634f5ced73098379ba3370f356aacff0ec
carlog1566 <cgprojects2508@gmail.com>
2025-12-17 00:03:49 -0800

    Set up NN model

--- FlappyBirdAI/model.py
+++ FlappyBirdAI/model.py
@@ -46,4 +46,35 @@ class QTrainer:
 
     # TRAIN STEP FUNCTION
     def train_step(self, state, action, reward, next_state, done):
-        pass
+        # Convert all inputs to tensors (so that the NN can work with it)
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+        reward = torch.tensor(reward, dtype=torch.float)
+        
+        # Convert each tensor from a 1D sample to a batch size of 1 (so that it can be processed by the NN)
+        if len(state.shape) == 1:
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            done = (done, )
+
+        # Predict Q-values, copy for loss calculation
+        pred = self.model(state)
+        target = pred.clone()
+
+        # Update Q-values using Bellman Equation
+        for i in range(len(done)):
+            Q_new = reward[i]
+            if not done[i]:
+                Q_new = reward[i] + self.gamma * torch.max(self.model(next_state[i]))
+
+            target[i][torch.argmax(action).item()] = Q_new
+
+        # Reset gradients, compute loss via mean squared error, backpropagate loss, update weights
+        self.optimizer.zero_grad()
+        loss = self.criterion(target, pred)
+        loss.backward()
+        self.optimizer.step()
+
